5267728119812c15b8132a0d9d3c1cd66411becc
florisv567-tech <florisv567@gmail.com>
2025-10-21 09:27:40 +0700

    Add files via upload

---
+++ olah.nilai.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+data siswa{
+    string nama;
+    string kelas;
+    string NISN;
+    float ipa, ips, matematika;
+    float NilaiRataRataSiswa;
+}
